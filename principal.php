3f88897c827cdc16e316cef7ea75eef1a99c5984
Luan Oliveira <luanoliveira35@Hotmail.com>
2022-12-26 23:24:55 -0300

    Upload to GitHub

--- principal.php
+++ principal.php
@@ -11,7 +11,7 @@
 </script>
 </head>
 <body>
-
+<!-- Posicione esta tag no cabeçalho ou imediatamente antes da tag de fechamento do corpo. -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
